f417c5c9f980cd55985ee1d91674e44b6d0d7f58
totoro2003 <39471893+totoro2003@users.noreply.github.com>
2018-12-23 01:31:52 -0500

    Update surface_management.cpp
    added surface optimization

--- surface_management.cpp
+++ surface_management.cpp
@@ -22,6 +22,27 @@ surface_management::surface_management( std::string file_name )
     }
 }
 
+void surface_management::optimize_surface ( SDL_Surface * screen_surface )
+{
+    SDL_Surface * optimized_surface = SDL_ConvertSurface 
+    (
+        surface,
+        screen_surface->format,
+        0
+    );
+    
+    if ( optimized_surface == NULL )
+    {
+        std::cout << "SDL was unable to optimize the surface. error: "
+        << SDL_GetError();
+    }
+    else 
+    {
+        surface = optimized_surface;
+    }
+    //SDL_FreeSurface( optimized_surface );
+}
+
 SDL_Surface * surface_management::get_surface_instance() 
 {
     return surface;
