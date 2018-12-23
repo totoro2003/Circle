e24fed384e9d5a8efa61f319f02df6e9e49148f3
totoro2003 <39471893+totoro2003@users.noreply.github.com>
2018-12-23 01:32:51 -0500

    Update surface_management.h
    added surface optimization

--- surface_management.h
+++ surface_management.h
@@ -25,6 +25,8 @@ public:
     
     surface_management & operator=( const surface_management& new_class ) = delete;
     
+    void optimize_surface ( SDL_Surface * screen_surface );
+    
     SDL_Surface * get_surface_instance();
     
     ~surface_management();
